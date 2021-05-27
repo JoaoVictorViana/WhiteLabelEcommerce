64a7111126fd9887052971e90a47f27b7230f33b
João Victor <jv.duarte.viana@gmail.com>
2021-05-27 12:21:13 -0300

    Initial commit

---
+++ wp-content/themes/whitelabelecommerce/header.php
@@ -0,0 +1,18 @@
+<?php declare( strict_types = 1 );
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+    <h1>Theme success!!</h1>
---
+++ wp-content/themes/whitelabelecommerce/index.php
@@ -0,0 +1,10 @@
+<?php declare( strict_types = 1 );
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+get_header();
+the_content();
+get_footer();
+?>
